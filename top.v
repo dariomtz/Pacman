973e1881998aa43903690c296b7552ab55a31be2
Dario Martinez <gusdamtzr@gmail.com>
2020-04-24 13:40:17 -0500

    begin

--- top.v
+++ top.v
@@ -4,11 +4,12 @@
 // Engineer:  dariomtz
 //////////////////////////////////////////////////////////////////////////////////
 module top(
-    input [3:0] row,
-	 input clk,
-	 output  [7:0] leds,
-    output reg [3:0] column
-    
+	input clk,
+	output vga_hs,
+	output vga_vs,
+    output [3:0] vga_r,
+	output [3:0] vga_g,
+	output [3:0] vga_b
     );
 
 	reg [1:0] barrido;	
@@ -22,13 +23,10 @@ module top(
 		begin
 			barrido = barrido +1;
 			case ( barrido)
-				0: column <='b0001;
-				1: column <='b0010;
-				2: column <='b0100;
-				3:begin
-					column <='b1000;
-					// barrido <=0;
-				  end
+				0: column <='1110;
+				1: column <='b1101;
+				2: column <='b1011;
+				3: column <='b0111;
 				default: column <=0;
 			endcase
 		end
