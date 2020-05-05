fa346da79d1f9d58441c6761452a75cb42ec2e08
Dario Martinez <gusdamtzr@gmail.com>
2020-05-05 15:22:07 -0500

    comment correction

--- ClkRedu.v
+++ ClkRedu.v
@@ -6,27 +6,27 @@
 module ClkRedu(
 	input clk,
 	input reset,
-	output reg ClkRedu //para bocina: puerto A PIN1: B2
+	output reg ClkRedu //slower clock
     );
 
-reg [24:0] conteo; //se definen 25 bits para el conteo porque 2^25 = 33Millones
-always @(posedge clk, posedge reset)
-	begin
-		if(reset)
-			begin
-				conteo <= 0; //reiniciar la cuenta
-				ClkRedu <= 0; //reiniciar la salida
-			end
-		else
-			begin
-				conteo <= conteo + 1;
-				if(conteo == 833_334) //contar hasta 25 millones para hacer Toggleo cada 0.5s
-					begin
-						ClkRedu <= ClkRedu + 1; //Togglea la salida
-						conteo <=0; //reinicia la cuenta
-					end
-			end
-	end
+	reg [19:0] count; //20 bits to fit 833_334
+	always @(posedge clk, posedge reset)
+		begin
+			if(reset)
+				begin
+					count <= 0; 
+					ClkRedu <= 0; 
+				end
+			else
+				begin
+					count <= count + 1;
+					if(count == 833_334) //count to 833_334 to toggle 60 times per second
+						begin
+							ClkRedu <= ClkRedu + 1; //Toggle output
+							count <=0; //restart count
+						end
+				end
+		end
 
 
 
